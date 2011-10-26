d3683d4c04d270ad052802eacacf83ed8cb75201
Phil Ostler <philostler@gmail.com>
2011-10-26 22:58:04 +0100

    Test namespace added.

--- spec/job/capability_spec.rb
+++ spec/job/capability_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Alloy::Job::Capability do
-  before(:each) { @model = Capability.new }
+  before(:each) { @model = Test::Alloy::Job::Capability.new }
 
   it { @model.should be_a Java::java.lang.Runnable }
   it { @model.should_not allow_public_access_for_methods :run }
--- spec/support/classes.rb
+++ spec/support/classes.rb
@@ -1,3 +1,16 @@
-# Capability
-Object.const_set "Capability", Class.new
-Capability.send :include, Alloy::Job::Capability
+# Test::Alloy
+Object.const_set("Test", Class.new).const_set("Alloy", Class.new)
+
+# Test::Alloy::Core
+Test::Alloy.const_set("Core", Class.new)
+
+# Test::Alloy::Core::Limit
+Test::Alloy::Core.const_set("Limit", Class.new)
+Test::Alloy::Core::Limit.send :extend, Alloy::Core::Limit
+
+# Test::Alloy::Job
+Test::Alloy.const_set("Job", Class.new)
+
+# Test::Alloy::Job::Capability
+Test::Alloy::Job.const_set("Capability", Class.new)
+Test::Alloy::Job::Capability.send :include, Alloy::Job::Capability
