69ce7acd5187ab380350d9ad4853f15c1234918f
Phil Ostler <philostler@gmail.com>
2011-10-28 01:19:03 +0100

    Added default value and nil check.

--- lib/alloy/core/limit.rb
+++ lib/alloy/core/limit.rb
@@ -1,13 +1,17 @@
 module Alloy
   module Core
     module Limit
-      @@limits = Hash.new
+      @@limits = Hash.new 10
 
       def limit queue, limit = nil
         raise ArgumentError, "limit cannot be less than 1" if limit.to_i < 1 unless limit.nil?
 
         if limit.nil?
-          @@limits[queue]
+          if @@limits.has_key? queue
+            @@limits[queue]
+          else
+            return nil
+          end
         else
           @@limits[queue] = limit.to_i
         end
--- spec/core/limit_spec.rb
+++ spec/core/limit_spec.rb
@@ -6,7 +6,7 @@ describe Alloy::Core::Limit do
   it { @model.should allow_public_access_for_methods :limit }
 
   describe ".limit" do
-    it "should return the limit" do
+    it "should return the specified queue limit" do
       @model.limit example.description, 7
 
       @model.limit(example.description).should be 7
@@ -17,7 +17,7 @@ describe Alloy::Core::Limit do
     end
 
     context "when a limit is specified" do
-      it "should set limit on the specified queue" do
+      it "should set the specified queue limit" do
         @model.limit example.description, 10
 
         @model.limit(example.description).should be 10
@@ -27,8 +27,10 @@ describe Alloy::Core::Limit do
         @model.limit(example.description, 10).should be 10
       end
 
-      it "should raise an argument error if the specified limit is less than one" do
-        expect { @model.limit example.description, 0 }.to raise_error ArgumentError, "limit cannot be less than 1"
+      context "when limit is less than one" do
+        it "should raise an argument error" do
+          expect { @model.limit example.description, 0 }.to raise_error ArgumentError, "limit cannot be less than 1"
+        end
       end
     end
   end
