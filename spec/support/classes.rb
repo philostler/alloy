bc7ddb61bc127c9f3a0531925aa67ae4495450f8
Phil Ostler <philostler@gmail.com>
2011-10-28 01:19:41 +0100

    RunOnce test class added.

--- spec/support/classes.rb
+++ spec/support/classes.rb
@@ -8,6 +8,10 @@ Test::Alloy.const_set("Core", Class.new)
 Test::Alloy::Core.const_set("Limit", Class.new)
 Test::Alloy::Core::Limit.send :extend, Alloy::Core::Limit
 
+# Test::Alloy::Core::RunOnce
+Test::Alloy::Core.const_set("RunOnce", Class.new)
+Test::Alloy::Core::RunOnce.send :extend, Alloy::Core::RunOnce
+
 # Test::Alloy::Job
 Test::Alloy.const_set("Job", Class.new)
 
