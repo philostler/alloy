b9fcf16217f2bbe1ac038cfd3da819c3b6c91d6d
Phil Ostler <philostler@gmail.com>
2011-10-28 01:18:08 +0100

    RunOnce module added.

--- lib/alloy.rb
+++ lib/alloy.rb
@@ -1,9 +1,11 @@
 require "alloy/version"
 
 require "alloy/core/limit"
+require "alloy/core/run_once"
 
 require "alloy/job/capability"
 
 module Alloy
   extend Alloy::Core::Limit
+  extend Alloy::Core::RunOnce
 end
