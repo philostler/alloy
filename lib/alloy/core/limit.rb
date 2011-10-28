62a6b9bbd5b19d9dca58a06ac1a0e1a5bb6fc4ac
Phil Ostler <philostler@gmail.com>
2011-10-28 01:26:18 +0100

    Improved syntax readability.

--- lib/alloy/core/limit.rb
+++ lib/alloy/core/limit.rb
@@ -6,14 +6,10 @@ module Alloy
       def limit queue, limit = nil
         raise ArgumentError, "limit cannot be less than 1" if limit.to_i < 1 unless limit.nil?
 
-        if limit.nil?
-          if @@limits.has_key? queue
-            @@limits[queue]
-          else
-            return nil
-          end
-        else
+        if limit
           @@limits[queue] = limit.to_i
+        else
+          @@limits[queue] if @@limits.has_key? queue
         end
       end
     end
