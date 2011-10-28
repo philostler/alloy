678663fbc7d46748a14965542f4e1af765b08b50
Phil Ostler <philostler@gmail.com>
2011-10-28 19:37:58 +0100

    Additional require and includes.

--- lib/alloy.rb
+++ lib/alloy.rb
@@ -1,11 +1,15 @@
 require "alloy/version"
 
+require "alloy/core/common"
 require "alloy/core/limit"
 require "alloy/core/run_once"
+require "alloy/core/schedule"
 
 require "alloy/job/capability"
 
 module Alloy
+  extend Alloy::Core::Common
   extend Alloy::Core::Limit
   extend Alloy::Core::RunOnce
+  extend Alloy::Core::Schedule
 end
