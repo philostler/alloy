027e7e0576d5d08157a4215cea08ea85a8a656e1
Phil Ostler <philostler@gmail.com>
2011-10-29 12:21:07 +0100

    Added additional modules.

--- spec/support/classes.rb
+++ spec/support/classes.rb
@@ -4,6 +4,10 @@ Object.const_set("Test", Class.new).const_set("Alloy", Class.new)
 # Test::Alloy::Core
 Test::Alloy.const_set("Core", Class.new)
 
+# Test::Alloy::Core::Common
+Test::Alloy::Core.const_set("Common", Class.new)
+Test::Alloy::Core::Common.send :extend, Alloy::Core::Common
+
 # Test::Alloy::Core::Limit
 Test::Alloy::Core.const_set("Limit", Class.new)
 Test::Alloy::Core::Limit.send :extend, Alloy::Core::Limit
@@ -12,6 +16,11 @@ Test::Alloy::Core::Limit.send :extend, Alloy::Core::Limit
 Test::Alloy::Core.const_set("RunOnce", Class.new)
 Test::Alloy::Core::RunOnce.send :extend, Alloy::Core::Common
 Test::Alloy::Core::RunOnce.send :extend, Alloy::Core::RunOnce
+
+# Test::Alloy::Core::Schedule
+Test::Alloy::Core.const_set("Schedule", Class.new)
+Test::Alloy::Core::Schedule.send :extend, Alloy::Core::Common
+Test::Alloy::Core::Schedule.send :extend, Alloy::Core::Schedule
 
 # Test::Alloy::Job
 Test::Alloy.const_set("Job", Class.new)
