a2d27477cf11afaf128f3f6ffe6970a6a56253e9
Phil Ostler <philostler@gmail.com>
2011-11-02 22:01:24 +0000

    Modified for new classes.

--- spec/alloy_spec.rb
+++ spec/alloy_spec.rb
@@ -3,9 +3,5 @@ require "spec_helper"
 describe Alloy do
   it { Alloy::VERSION.should == "0.1.0" }
 
-  it { should be_a Alloy::Core::Common }
-  it { should be_a Alloy::Core::DefaultLimit }
-  it { should be_a Alloy::Core::Limit }
-  it { should be_a Alloy::Core::RunOnce }
-  it { should be_a Alloy::Core::Schedule }
+  it { should be_a Alloy::Core::Size }
 end
--- spec/support/classes.rb
+++ spec/support/classes.rb
@@ -4,28 +4,13 @@ Object.const_set("Test", Class.new).const_set("Alloy", Class.new)
 # Test::Alloy::Core
 Test::Alloy.const_set("Core", Class.new)
 
-# Test::Alloy::Core::DefaultLimit
-Test::Alloy::Core.const_set("DefaultLimit", Class.new)
-Test::Alloy::Core::DefaultLimit.send :extend, Alloy::Core::DefaultLimit
-
-# Test::Alloy::Core::Limit
-Test::Alloy::Core.const_set("Limit", Class.new)
-Test::Alloy::Core::Limit.send :extend, Alloy::Core::DefaultLimit
-Test::Alloy::Core::Limit.send :extend, Alloy::Core::Limit
-
-# Test::Alloy::Core::RunOnce
-Test::Alloy::Core.const_set("RunOnce", Class.new)
-Test::Alloy::Core::RunOnce.send :extend, Alloy::Core::Common
-Test::Alloy::Core::RunOnce.send :extend, Alloy::Core::RunOnce
-
-# Test::Alloy::Core::Schedule
-Test::Alloy::Core.const_set("Schedule", Class.new)
-Test::Alloy::Core::Schedule.send :extend, Alloy::Core::Common
-Test::Alloy::Core::Schedule.send :extend, Alloy::Core::Schedule
+# Test::Alloy::Core::Size
+Test::Alloy::Core.const_set("Size", Class.new)
+Test::Alloy::Core::Size.send :extend, Alloy::Core::Size
 
 # Test::Alloy::Job
 Test::Alloy.const_set("Job", Class.new)
 
-# Test::Alloy::Job::Capability
-Test::Alloy::Job.const_set("Capability", Class.new)
-Test::Alloy::Job::Capability.send :include, Alloy::Job::Capability
+# Test::Alloy::Job::Able
+Test::Alloy::Job.const_set("Able", Class.new)
+Test::Alloy::Job::Able.send :include, Alloy::Job::Able
