4da24e336a602d94c62ba80cfe0dbc5b00d63307
Phil Ostler <philostler@gmail.com>
2011-11-05 10:17:41 +0000

    Simplified syntax.

--- spec/core/size_spec.rb
+++ spec/core/size_spec.rb
@@ -1,18 +1,18 @@
 require "spec_helper"
 
 describe Alloy::Core::Size do
-  before(:all) { @clazz = Test::Alloy::Core::Size }
+  subject { Test::Alloy::Core::Size }
 
-  it { @clazz.should allow_public_access_for_methods :size }
+  it { subject.should allow_public_access_for_methods :size }
 
   describe ".size" do
     it "should return the size poro class" do
-      @clazz.size.should be Alloy::Poros::Size
+      subject.size.should be Alloy::Poros::Size
     end
 
     context "when a queue is specified" do
       it "should return a size poro instance" do
-        @clazz.size(:queue).should be_a Alloy::Poros::Size
+        subject.size(:queue).should be_a Alloy::Poros::Size
       end
     end
   end
--- spec/job/able_spec.rb
+++ spec/job/able_spec.rb
@@ -1,16 +1,16 @@
 require "spec_helper"
 
 describe Alloy::Job::Able do
-  before(:each) { @model = Test::Alloy::Job::Able.new }
+  subject { Test::Alloy::Job::Able.new }
 
-  it { @model.should be_a Java::java.lang.Runnable }
-  it { @model.should_not allow_public_access_for_methods :run }
+  it { subject.should be_a Java::java.lang.Runnable }
+  it { subject.should_not allow_public_access_for_methods :run }
 
   describe "#run" do
     it "should invoke execute" do
-      @model.should_receive :execute
+      subject.should_receive :execute
 
-      @model.send :run
+      subject.send :run
     end
   end
 end
