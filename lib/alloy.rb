d0a24a764891576310c40fc6e9788c2e97da935e
Phil Ostler <philostler@gmail.com>
2011-11-05 10:16:53 +0000

    Additional new modules and poros.

--- lib/alloy.rb
+++ lib/alloy.rb
@@ -1,11 +1,19 @@
 require "alloy/version"
 
+require "alloy/poros/run"
+require "alloy/poros/schedule"
 require "alloy/poros/size"
 
+require "alloy/core/base"
+require "alloy/core/run"
+require "alloy/core/schedule"
 require "alloy/core/size"
 
 require "alloy/job/able"
 
 module Alloy
+  extend Alloy::Core::Base
+  extend Alloy::Core::Run
+  extend Alloy::Core::Schedule
   extend Alloy::Core::Size
 end
