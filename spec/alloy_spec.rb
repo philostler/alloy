d99128de8b13bf22951b417e055bf7f95287d7e7
Phil Ostler <philostler@gmail.com>
2011-11-13 17:10:36 +0000

    Examples for new modules.

--- spec/alloy_spec.rb
+++ spec/alloy_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 describe Alloy do
   it { Alloy::VERSION.should == "0.1.0" }
 
-  it { should be_a Alloy::Core::Base }
-  it { should be_a Alloy::Core::Run }
-  it { should be_a Alloy::Core::Schedule }
+  it { should be_a Alloy::Core::Executors }
+  it { should be_a Alloy::Core::Jobs }
   it { should be_a Alloy::Core::Size }
+  it { should be_a Alloy::Core::Strategies }
 end
