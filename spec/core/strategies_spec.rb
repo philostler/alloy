d5e6203967c5e09dee20eccf538cf5693f7f39f1
Phil Ostler <philostler@gmail.com>
2011-11-14 21:38:08 +0000

    Switched to use new should_execute helper.

--- spec/core/strategies_spec.rb
+++ spec/core/strategies_spec.rb
@@ -2,18 +2,18 @@ require "spec_helper"
 
 describe Alloy::Core::Strategies do
   subject { Test::Alloy::Core::Strategies }
-  let(:clazz) { TestJob }
+  let(:clazz) { ExecutionInfoJob }
   let(:job) { clazz.new }
 
   it { subject.should allow_public_access_for_methods :run, :schedule }
 
   describe ".run" do
     it "should execute the specified job once" do
-      job.should_receive(:execute).once
-
       subject.should_receive(:create_job).once.and_return job
 
       subject.run(clazz).now
+
+      should_execute(job).once
     end
     it "should execute the specified job upon the job's specified queue" do
       queue = clazz.instance_variable_get :@queue
@@ -27,66 +27,56 @@ describe Alloy::Core::Strategies do
 
       subject.run(clazz).now
 
-      sleep_until(job).done
-
+      should_execute(job).once
       job.thread.id.should_not be this_thread.id
     end
   end
 
   describe ".schedule" do
     it "should execute the specified job multiple times" do
-      job.should_receive(:execute).at_least(2).times
-
       subject.should_receive(:create_job).once.and_return job
 
       subject.schedule(clazz).every 1
 
-      sleep 2
+      should_execute(job).twice
     end
     it "should execute the specified job upon the job's specified queue" do
       queue = clazz.instance_variable_get :@queue
 
       subject.should_receive(:get_executor).with queue
 
-      subject.schedule(clazz).every 10
+      subject.schedule(clazz).every 1
     end
     it "should execute the specified job upon a seperate thread" do
       subject.should_receive(:create_job).once.and_return job
 
       subject.schedule(clazz).every 1
 
-      sleep_until(job).done
-
+      should_execute(job).twice
       job.thread.id.should_not be this_thread.id
     end
     it "should execute the specified job once within the 'in' duration" do
-      job.should_receive(:execute).at_most(1).times
-
       subject.should_receive(:create_job).once.and_return job
 
       subject.schedule(clazz).in(1).every 60
 
-      sleep 2
+      should_execute(job).within(2).seconds.once
     end
     it "should execute the specified job once within the 'every' duration" do
-      job.should_receive(:execute).at_most(2).times
-
       subject.should_receive(:create_job).once.and_return job
 
-      subject.schedule(clazz).every 2
+      subject.schedule(clazz).every 1
 
-      sleep 3
+      should_execute(job).within(2).seconds.twice
     end
 
     context "when an 'in' duration is not specified" do
       it "should execute the specified job immediately" do
-        job.should_receive(:execute).at_most(2).times
-
         subject.should_receive(:create_job).once.and_return job
 
-        subject.schedule(clazz).every 10
+        subject.schedule(clazz).every 5
 
-        sleep 1
+        should_execute(job).within(2).seconds.once
       end
     end
     context "when an 'in' duration is less than one" do
