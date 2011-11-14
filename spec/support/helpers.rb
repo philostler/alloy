80dc132eed8853739fa31e0c10ce71d36768f73d
Phil Ostler <philostler@gmail.com>
2011-11-14 21:37:21 +0000

    New should_execute method providing job testing integrity.

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,13 +1,37 @@
-def sleep_until job
+def should_execute job
   Class.new do
+    include RSpec::Matchers
+
     def initialize job
       @job = job
+      @start_time = Time.now
+      @within = 10
     end
 
-    def done
-      while not @job.done
+    def once
+      while @job.execution_count.to_i < 1 && !timed_out
         sleep 0.1
       end
+      @job.execution_count.should be 1
+    end
+    def seconds
+      self
+    end
+    def twice
+      while @job.execution_count.to_i < 2 && !timed_out
+        sleep 0.1
+      end
+      @job.execution_count.should be 2
+    end
+    def within duration
+      @within = duration
+
+      self
+    end
+
+    private
+    def timed_out
+      true unless Time.now - @start_time < @within
     end
   end.new job
 end
